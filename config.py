a62904f9c129d45bc5de37e567219e2cc27bf7f0
liamnguyen97 <107618374+liamnguyen97@users.noreply.github.com>
2023-08-21 16:05:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,7 @@ class Config:
     def load_pretrained_model(self, model_checkpoint):
         model = BloomForCausalLM.from_pretrained(model_checkpoint)
         
-       
-        return model.to(self.device)
+        return model
     
     def add_lora(self, model, r: int, lora_alpha: int, lora_dropout: float):
         lora_config = LoraConfig(r = r,
